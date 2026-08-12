021d7e5520da3389d08c3c705a3513ef01620094
Mox <tenmaken246@gmail.com>
2026-08-12 22:49:15 +0300

    optimization

--- dags/scripts/transform.py
+++ dags/scripts/transform.py
@@ -20,11 +20,13 @@ properties = {
     "driver": DRIVER
 }
 
-def sum_stars():
-    spark = SparkSession.builder.master("local[*]").appName('Transform_repo') \
+spark = SparkSession.builder.master("local[*]").appName('Transform_repo') \
         .config("spark.jars.packages", "org.postgresql:postgresql:42.7.1").getOrCreate()
 
-    df = spark.read.jdbc(JDBC_URL, DBTABLE, properties=properties)
+df = spark.read.jdbc(JDBC_URL, DBTABLE, properties=properties).cache()
+# я подумала, что это хорошая идея добавить cahce(), чтобы не выполняеть постоянно чтение dataFrame :)
+
+def sum_stars():
 
     stars_sum = df.groupBy(F.col("owner_login"))\
         .agg(F.sum('stars').alias("sum_stars"), F.count("id").alias("cnt_repo"), F.avg("forks").alias("avg_forks"))\
@@ -32,13 +34,8 @@ def sum_stars():
 
     stars_sum.show()
 
-    spark.stop()
 
 def forks_to_stars():
-    spark = SparkSession.builder.master("local[*]").appName('Transform_repo') \
-        .config("spark.jars.packages", "org.postgresql:postgresql:42.7.1").getOrCreate()
-
-    df = spark.read.jdbc(JDBC_URL, DBTABLE, properties=properties)
 
     forks_stars = df.groupBy(F.col("owner_login"))\
         .agg(F.round(F.sum('forks') / F.sum('stars'),5).alias("forks_to_stars"),
@@ -47,19 +44,9 @@ def forks_to_stars():
 
     forks_stars.show()
 
-    spark.stop()
-
 def dynamics_stars():
     """
     select owner_login, created_at, stars, coalesce(stars - lag(stars) over(partition by id order by created_at ), 0) as cnt_stars
     from repo
     order by owner_login, created_at;
     """
-    spark = SparkSession.builder.master("local[*]").appName('Transform_repo') \
-        .config("spark.jars.packages", "org.postgresql:postgresql:42.7.1").getOrCreate()
-
-    df = spark.read.jdbc(JDBC_URL, DBTABLE, properties=properties)
-
-    
-
-    spark.stop()
